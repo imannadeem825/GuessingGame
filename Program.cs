8245e77553bc59075d005fddf67c7cfbee25666d
Iman Nadeem <iman.nadeem.825@gmail.com>
2021-03-02 13:54:34 -0600

    phase 2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
 
-namespace GuessingGame
+Main();
+
+void Main()
 {
-    class Program
+    Console.Write("Guess the secret number: ");
+    GuessNumber();
+}
+
+
+void GuessNumber()
+{
+    int secretNumber = 42;
+    string userGuess = Console.ReadLine();
+    int parsedAnswer = Int32.Parse(userGuess);
+
+    if (string.IsNullOrWhiteSpace(userGuess))
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        Console.WriteLine("Take a guess, won't ya?");
+    }
+    else if (parsedAnswer == secretNumber)
+    {
+        Console.WriteLine("Nice job, you got it!");
+    }
+    else 
+    {
+        Console.WriteLine("Too bad, wrong answer!");
     }
 }
+
+
+
+
+
+
+// void GuessNumber()
+// {
+//     bool userGuess = ProgramAsks("Take a guess, won't ya?");
+//     if (userGuess) 
+//     {
+        
+//         string answer = Console.ReadLine();
+//         Console.Write(answer)
+//     }
+
+// }
+
+
+// bool ProgramAsks(string question)
+// {
+//     Console.Write($"{question}");
+//     string answer = Console.ReadLine();
+//     Console.Write(answer)
+// }
+
+
+// void ProgramSays(string message)
+// {
+//     Console.WriteLine($"{message}")
+// }
+
+
+
